4e1b5908d84484c70fcb834167b2c6d6cb86bc1a
Matt Wells <matt.wells@intelligentclaims.com>
2019-03-29 10:09:14 +0000

    Override RankedGradeBook's GetLetterGrade

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -1,4 +1,6 @@
 ﻿using GradeBook.Enums;
+using System;
+using System.Linq;
 
 namespace GradeBook.GradeBooks
 {
@@ -8,6 +10,28 @@ namespace GradeBook.GradeBooks
         {
             Type = GradeBookType.Ranked;
         }
-   
+
+        public override char GetLetterGrade(double averageGrade)
+        {
+            if (Students.Count < 5)
+            {
+                throw new InvalidOperationException();
+            }
+
+            var threshold = (int)Math.Ceiling(Students.Count * .2);
+
+            var grades = Students.OrderByDescending(e => e.AverageGrade).Select(e => e.AverageGrade).ToList();
+
+            char grade = 'A';
+            for (int i = 0; i < 4; i++) {
+                if (grades[(threshold * i)] <= averageGrade) {
+                    return grade;
+                }
+
+                grade++;
+            }
+
+            return 'F';
+        }
     }
 }
